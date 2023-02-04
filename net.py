f5bd8162a1e4f773ff62027260455f64d199f359
noobjam <74034851+noobjam@users.noreply.github.com>
2023-02-04 16:35:33 +0000

    Update net.py

--- net.py
+++ net.py
@@ -1,131 +1,231 @@
 import numpy as np
 
-def sigmoid(x):
-  # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
-  return 1 / (1 + np.exp(-x))
-
-def deriv_sigmoid(x):
-  # Derivative of sigmoid: f'(x) = f(x) * (1 - f(x))
-  fx = sigmoid(x)
-  return fx * (1 - fx)
-
-def mse_loss(y_true, y_pred):
-  # y_true and y_pred are numpy arrays of the same length.
-  return ((y_true - y_pred) ** 2).mean()
-
-class OurNeuralNetwork:
-  '''
-  A neural network with:
-    - 2 inputs
-    - a hidden layer with 2 neurons (h1, h2)
-    - an output layer with 1 neuron (o1)
-
-  *** DISCLAIMER ***:
-  The code below is intended to be simple and educational, NOT optimal.
-  Real neural net code looks nothing like this. DO NOT use this code.
-  Instead, read/run it to understand how this specific network works.
-  '''
-  def __init__(self):
-    # Weights
-    self.w1 = np.random.normal()
-    self.w2 = np.random.normal()
-    self.w3 = np.random.normal()
-    self.w4 = np.random.normal()
-    self.w5 = np.random.normal()
-    self.w6 = np.random.normal()
-
-    # Biases
-    self.b1 = np.random.normal()
-    self.b2 = np.random.normal()
-    self.b3 = np.random.normal()
-
-  def feedforward(self, x):
-    # x is a numpy array with 2 elements.
-    h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
-    h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
-    o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
-    return o1
-
-  def train(self, data, all_y_trues):
+class NeuralNetwork():
     '''
-    - data is a (n x 2) numpy array, n = # of samples in the dataset.
-    - all_y_trues is a numpy array with n elements.
-      Elements in all_y_trues correspond to those in data.
+    Neural Network object.
+
+    Args:
+        input_nodes (int): 
+            The size of the input layer of the ANN
+        hidden_nodes (int): 
+            The size of the hidden layer of the ANN
+        output_nodes (int): 
+            The size of the output layer of the ANN
+        learning_rate (float): 
+            The learning rate in (0, 1]
+                
+    Attributes:
+        input_nodes (int): 
+            The size of the input layer of the ANN
+        hidden_nodes (int): 
+            The size of the hidden layer of the ANN
+        output_nodes (int): 
+            The size of the output layer of the ANN
+        lr (float): 
+            The learning rate in (0, 1]
+        activation_function (function):
+            The activation function of the ANN (sigmoid)
+        weights_input_to_hidden (numpy array 2D): 
+            The wights from the input to the hidden layer.
+        weights_hidden_to_output (numpy array 2D): 
+            The wights from the hidden to the output layer.
+                                
+    Methods:
+        train():
+            Train the network on batch of features and targets. 
+        __feedForwrad():
+            Implement feed forward, calculate the hidden layer and final outputs
+        __backpropagation():
+            Backpropagation implementation.
+        __update_weights():
+            Update weights on gradient descent step.
+        predict:
+            Run a forward pass through the network with input features and return
+            a prediction.
     '''
-    learn_rate = 0.1
-    epochs = 1000 # number of times to loop through the entire dataset
 
-    for epoch in range(epochs):
-      for x, y_true in zip(data, all_y_trues):
-        # --- Do a feedforward (we'll need these values later)
-        sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
-        h1 = sigmoid(sum_h1)
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        '''
+        Class constructor.
 
-        sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
-        h2 = sigmoid(sum_h2)
+        Args:
+            input_nodes (int): 
+                The size of the input layer of the ANN
+            hidden_nodes (int): 
+                The size of the hidden layer of the ANN
+            output_nodes (int): 
+                The size of the output layer of the ANN
+            learning_rate (float): 
+                The learning rate in (0, 1]
 
-        sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
-        o1 = sigmoid(sum_o1)
-        y_pred = o1
+        Raises:
+            -
 
-        # --- Calculate partial derivatives.
-        # --- Naming: d_L_d_w1 represents "partial L / partial w1"
-        d_L_d_ypred = -2 * (y_true - y_pred)
+        Returns:
+            -
+        '''
 
-        # Neuron o1
-        d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
-        d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
-        d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+        # Set number of nodes in input, hidden and output layers.
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
 
-        d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
-        d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+        # Initialize weights
+        self.weights_input_to_hidden = np.random.normal(0.0, self.input_nodes**-0.5, 
+                                       (self.input_nodes, self.hidden_nodes))
 
-        # Neuron h1
-        d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
-        d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
-        d_h1_d_b1 = deriv_sigmoid(sum_h1)
+        self.weights_hidden_to_output = np.random.normal(0.0, self.hidden_nodes**-0.5, 
+                                       (self.hidden_nodes, self.output_nodes))
+        self.lr = learning_rate
+        
+        # Set self.activation_function to the sigmoid
+        self.activation_function = lambda x : 1/(1+np.exp(-x))
+                    
 
-        # Neuron h2
-        d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
-        d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
-        d_h2_d_b2 = deriv_sigmoid(sum_h2)
+    def train(self, features, targets):
+        '''
+        Train the network on batch of features and targets. 
 
-        # --- Update weights and biases
-        # Neuron h1
-        self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
-        self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
-        self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+        Args:
+            features (numpy array 2D): 
+                Each row is one data record, each column is a feature
+            targets (numpy array 1D): 
+                Target values
+            
+        Raises:
+            -
 
-        # Neuron h2
-        self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
-        self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
-        self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
+        Returns:
+            -
+        '''
+        
+        n_records = features.shape[0]
+        delta_weights_i_h = np.zeros(self.weights_input_to_hidden.shape)
+        delta_weights_h_o = np.zeros(self.weights_hidden_to_output.shape)
+        
+        for X, y in zip(features, targets):
+            
+            final_outputs, hidden_outputs = self.__feedForwrad(X)
 
-        # Neuron o1
-        self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
-        self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
-        self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+            delta_weights_i_h, delta_weights_h_o = self.__backpropagation(final_outputs, hidden_outputs, X, y, 
+                                                                        delta_weights_i_h, delta_weights_h_o)
+        self.__update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
 
-      # --- Calculate total loss at the end of each epoch
-      if epoch % 10 == 0:
-        y_preds = np.apply_along_axis(self.feedforward, 1, data)
-        loss = mse_loss(all_y_trues, y_preds)
-        print("Epoch %d loss: %.3f" % (epoch, loss))
 
-# Define dataset
-data = np.array([
-  [-2, -1],  # Alice
-  [25, 6],   # Bob
-  [17, 4],   # Charlie
-  [-15, -6], # Diana
-])
-all_y_trues = np.array([
-  1, # Alice
-  0, # Bob
-  0, # Charlie
-  1, # Diana
-])
+    def __feedForwrad(self, X):
+        '''
+        Implement feed forward, calculate the hidden layer and final outputs
 
-# Train our neural network!
-network = OurNeuralNetwork()
-network.train(data, all_y_trues)
+        Args:
+            X (numpy array 2D): 
+                Each row is one data record, each column is a feature
+
+        Raises:
+            -
+
+        Returns:
+            final_outputs  (numpy array 2D): 
+                Signals from final output layer
+            hidden_outputs (numpy array 2D): 
+                Signals from hidden layer
+        '''
+
+        # Hidden layer
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden)   # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)  # signals from hidden layer
+
+        # Output layer
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)  # signals into final output layer
+        final_outputs = final_inputs  # signals from final output layer, the activation function is f(x) = x
+        
+        return final_outputs, hidden_outputs
+
+
+    def __backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
+        '''
+        Backpropagation implementation.
+
+        Args:
+            final_outputs: 
+                output from forward pass
+            y: 
+                target (i.e. label) batch
+            delta_weights_i_h: 
+                change in weights from input to hidden layers
+            delta_weights_h_o: 
+                change in weights from hidden to output layers
+
+        Raises:
+            -
+
+        Returns:
+            delta_weights_i_h (float): 
+                Weight step (input to hidden)
+            delta_weights_h_o (float): 
+                Weight step (hidden to output)
+        '''
+
+        # Output error
+        # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs
+
+        # activation function of the output layer is f(x) = x, where f'(x) = 1
+        output_error_term = error * 1.
+
+        # Calculate the hidden layer's contribution to the error
+        hidden_error = np.dot(self.weights_hidden_to_output, output_error_term)
+        hidden_error_term = hidden_error * hidden_outputs * (1-hidden_outputs)
+
+        # Weight step (input to hidden)
+        delta_weights_i_h += X[:, None] * hidden_error_term
+
+        # Weight step (hidden to output)
+        delta_weights_h_o += output_error_term[:,None] * hidden_outputs[:, None]
+
+        return delta_weights_i_h, delta_weights_h_o
+
+
+    def __update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
+        '''
+        Update weights on gradient descent step.
+
+        Args:
+            delta_weights_i_h: 
+                Change in weights from input to hidden layers
+            delta_weights_h_o: 
+                Change in weights from hidden to output layers
+            n_records: 
+                Number of records
+
+        Raises:
+            -
+
+        Returns:
+            -
+        '''
+
+        # update hidden-to-output weights with gradient descent step
+        self.weights_hidden_to_output += self.lr*delta_weights_h_o/n_records
+
+        # update input-to-hidden weights with gradient descent step
+        self.weights_input_to_hidden += self.lr*delta_weights_i_h/n_records
+
+
+    def predict(self, features):
+        '''
+        Run a forward pass through the network with input features and return
+        a prediction.
+
+        Args:
+            features (numpy array 2D): 
+                Each row is one data record, each column is a feature
+
+        Raises:
+            -
+
+        Returns:
+            prediction (numpy array 1D): 
+                Predictions for the input data.
+        '''
+
+        return self.__feedForwrad(features)[0]
